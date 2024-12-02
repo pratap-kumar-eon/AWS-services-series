090e7f4200192dd28e79f33fc6be56688e1cf790
Pratap Git <pratapkumarnayak09@gmail.com>
2024-12-02 18:12:55 +0530

    use of putobject of aws s3 service in node js

--- S3-nodejs/index.js
+++ S3-nodejs/index.js
@@ -1,12 +1,12 @@
-const { S3Client,GetObjectCommand } = require('@aws-sdk/client-s3')
+const { S3Client,GetObjectCommand,PutObjectCommand } = require('@aws-sdk/client-s3')
 
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 const s3Client = new S3Client({
     region: 'ap-south-1',
     credentials:{
-        accessKeyId:'',
-        secretAccessKey:''
+        accessKeyId:'AKIAWOOXTS7OV4LSXOHW',
+        secretAccessKey:'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'
     },
 });
 
@@ -19,7 +19,31 @@ async function getObjectURL(key){
     return url;
 }
 
-getObjectURL('0-cl7fc6pt1MHjIF4K.png')
+async function putObjectURL(filename,contentType){
+    const command  = new PutObjectCommand({
+        Bucket:'s3-nodejs-private',
+        Key:`uploads/user-uploads/${filename}`,
+        ContentType:contentType,
+    });
+
+    const url = await getSignedUrl(s3Client,command)
+    return url;
+}
+
+// code for get object
+getObjectURL('uploads/user-uploads/image-1733142960515.png')
+.then((url)=>{
+    console.log('URl is',url);
+    
+})
+.catch((error)=>{
+    console.log('Err',error);
+    
+})
+
+//code for put object
+
+putObjectURL(`image-${Date.now()}.png`,'image/png')
 .then((url)=>{
     console.log('URl is',url);
     
